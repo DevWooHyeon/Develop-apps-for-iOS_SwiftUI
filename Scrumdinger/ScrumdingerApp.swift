a84a776d274c035c8abcf16c520936d348e144df
Toy <woohyeon3382@naver.com>
2023-12-09 00:50:00 +0900

    Create New Project

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Toy on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
