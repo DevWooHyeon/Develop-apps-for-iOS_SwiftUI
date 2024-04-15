625f6b615cc9b570420c9737be50998f41cd242c
Nova <woohyeon3382@naver.com>
2024-04-15 19:33:13 +0900

    Section3: Add accessibility modifiers

--- Scrumdinger/DetailEditView.swift
+++ Scrumdinger/DetailEditView.swift
@@ -20,10 +20,12 @@ struct DetailEditView: View {
                     Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
                         Text("Length")
                     }
+                    .accessibilityValue("\(scrum.lengthInMinutes) minutes")
                     
                     Spacer()
                     
                     Text("\(scrum.lengthInMinutes) minutes")
+                        .accessibilityHidden(true)
                 }
             } header: {
                 Text("Meeting Info")
@@ -51,6 +53,7 @@ struct DetailEditView: View {
                     }
                 } label: {
                     Image(systemName: "plus.circle.fill")
+                        .accessibilityLabel("Add attendee")
                 }
                 .disabled(newAttendeeName.isEmpty)
             }
